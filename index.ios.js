6e4e95b52b55fa5461a6dee56d5e2ead5289b084
akash_koul <akash.koul@mail.click-labs.com>
2018-07-17 15:36:24 +0530

    react code pushed tto repo

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+AppRegistry.registerComponent('snowflake', () => App);
